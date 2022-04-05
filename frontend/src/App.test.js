86d0736ee703e55f889f1511ac9d6dcf88575457
breadmaking <liquidgoldlabel@gmail.com>
2022-04-05 16:30:02 +0100

    adding done

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Pantrypal/i);
+  expect(heading).toBeInTheDocument();
 });
+
