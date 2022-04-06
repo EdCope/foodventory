24e55079af41974b77a66d1e7d72ddf23ffbb4c7
Jack <winterschladen.j@gmail.com>
2022-04-06 15:12:09 +0100

    Create a new pantry unique to user on account creation
    Co-authored-by: EdCope <EdCope@users.noreply.github.com>

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,19 @@
 const User = require("../models/user");
+const Pantry = require("../models/pantry")
 
 const UsersController = { 
 
   Create: async (req, res) => {
     try{
+      req.body.pantry = new Pantry()
+      
       const user = new User(
         req.body
       )
       await user.save()   
-      res.json({'message': 'Account created'})
+        res.json({'message': 'Account created'})
     } catch (err) {
-      res.json({'message': err})
+        res.json({'message': err})
     }
   }
     
