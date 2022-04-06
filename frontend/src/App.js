b416dc51d62549d605697a66d2aca3d798cb4d33
Jack <winterschladen.j@gmail.com>
2022-04-06 11:46:52 +0100

    Created sign up form that takes e-mail on the front-end
    Co-authored-by: EdCope <EdCope@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {ListIngredients} from './components/listIngredients';
 import {AddIngredient} from './components/addIngredient';
 import {SignUp} from './components/signUp'
--- frontend/src/components/signUp.js
+++ frontend/src/components/signUp.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const SignUp = () => {
-  return <h2>Sign Up</h2>;
+  
+  const [email, setEmail] = useState('')
+  
+  const handleChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(email)
+  }
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+        <form id="sign-up" onSubmit={submitHandler}> 
+          <input type="email" id="email-form" placeholder="E mail" value={email} onChange={handleChange}></input>
+          <button type='submit' id="sign-up-button">Sign up</button>
+        </form>
+    </div>
+  )
 }
