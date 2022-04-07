2afeb3686f231b32aa2c828d98897b2ab3524f48
Ed <ed321@hotmail.co.uk>
2022-04-07 21:45:01 +0100

    Login authorisation responds with a jsonwebtoken from the server on password match

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const testRouter = require('./routes/testfile');
 const pantryRouter = require('./routes/pantry');
 const userRouter = require('./routes/user');
+const authRouter = require('./routes/auth');
 
 const cors = require('cors');
 
@@ -13,5 +14,6 @@ app.use(cors({ origin: true, credentials: true }));
 app.use('/', testRouter);
 app.use('/user', userRouter);
 app.use('/pantry', pantryRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
