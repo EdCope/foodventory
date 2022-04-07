4dabdef06ca82930337e258d61174dcda1fc2ee4
Jack <winterschladen.j@gmail.com>
2022-04-07 16:58:22 +0100

    Ingredient model now takes an object

--- models/ingredient.js
+++ models/ingredient.js
@@ -5,15 +5,15 @@ const IngredientSchema = new mongoose.Schema({
   name: String,
 
 })
-IngredientSchema.virtual('type').get(() => {
-  // this url searches by name
-  fetch(`https://api.spoonacular.com//food/ingredients/search?query=${this.name}&number=1`)
-  .then(response => response.json())
-  .then(data => console.log('id search returns: ', data))
-  })
-  // this url searches information by id
-  // `https://api.spoonacular.com//food/ingredients/:id/information?amount=130&unit=grams`
-  // return categoryPath
+// IngredientSchema.virtual('type').get(() => {
+//   // this url searches by name
+//   fetch(`https://api.spoonacular.com//food/ingredients/search?query=${this.name}&number=1`)
+//   .then(response => response.json())
+//   .then(data => console.log('id search returns: ', data))
+//   })
+//   // this url searches information by id
+//   // `https://api.spoonacular.com//food/ingredients/:id/information?amount=130&unit=grams`
+//   // return categoryPath
 
 const Ingredient = mongoose.model("Ingredient", IngredientSchema);
 
