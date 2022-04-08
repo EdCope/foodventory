72db2e16153527b188d917bba80ee85f0996c55c
breadmaking <liquidgoldlabel@gmail.com>
2022-04-08 11:54:34 +0100

    Fixed alert

--- controllers/pantry.js
+++ controllers/pantry.js
@@ -1,5 +1,5 @@
-const Pantry = require('../models/pantry');
-const Ingredient = require('../models/ingredient');
+const Pantry = require("../models/pantry");
+const Ingredient = require("../models/ingredient");
 
 const PantriesController = {
   Add: (req, res) => {
@@ -8,35 +8,43 @@ const PantriesController = {
       .then((pantries) => {
         if (pantries.length === 0) {
           const pantry = new Pantry();
-          const ingredient = new Ingredient({name:req.body.ingredient})
-          ingredient.save()
-          console.log('the req.body.ingredient is saved as: ', req.body.ingredient)
+          const ingredient = new Ingredient({
+            name: req.body.ingredient,
+            pantry: pantry,
+          });
+          ingredient.save();
           pantry.ingredients.push(ingredient);
-          pantry.save((err) => {
-            if (err) {
-              throw err;
-            }
-            res.json({ message: `${req.body.ingredient} successfully added` });
-          });
-        } else if (!pantries[0].ingredients.includes(req.body.ingredient)) {
-          const pantry = pantries[0];
-          const ingredient = new Ingredient({name:req.body.ingredient})
-          console.log(ingredient.type)
-          ingredient.save((err) => {
-            if (err) {
-              throw err;
-            }
-          });
-          console.log('the req.body.ingredient is saved as: ', req.body)
-          pantry.ingredients.push(ingredient);
-          pantry.save((err) => {
-            if (err) {
-              throw err;
-            }
-            res.json({ message: `${req.body.ingredient} successfully added` });
-          });
+          pantry.save();
+          res.json({ message: `${req.body.ingredient} successfully added` });
         } else {
-          res.json({ message: `${req.body.ingredient} already in pantry.` })
+          const pantry = pantries[0];
+          Ingredient.findOne({ pantry: pantry._id, name: req.body.ingredient })
+            .exec()
+            .then((ingredient) => {
+              if (ingredient === null) {
+                console.log("here");
+                const ingredient = new Ingredient({
+                  name: req.body.ingredient,
+                  pantry: pantry,
+                });
+                console.log(ingredient.type);
+                ingredient.save((err) => {
+                  if (err) {
+                    throw err;
+                  }
+                });
+                pantry.ingredients.push(ingredient);
+                pantry.save();
+                res.json({
+                  message: `${req.body.ingredient} successfully added`,
+                });
+              } else {
+                console.log("here");
+                res.json({
+                  message: `${req.body.ingredient} already in pantry.`,
+                });
+              }
+            });
         }
       });
   },
@@ -45,19 +53,18 @@ const PantriesController = {
     Pantry.find({})
       .exec()
       .then((pantry) => {
-        console.log(req.body)
-          Ingredient.deleteOne({_id: req.body.ingredient._id}, function(err) {
-            res.json({ message: `${req.body.ingredient.name} successfully removed` });
+        console.log(req.body);
+        Ingredient.deleteOne({ _id: req.body.ingredient._id }, function (err) {
+          res.json({
+            message: `${req.body.ingredient.name} successfully removed`,
           });
-          
-            
-          });
-      },
-
+        });
+      });
+  },
 
   GetAllIngredients: async (req, res) => {
     try {
-      const pantries = await Pantry.find({}).populate('ingredients');
+      const pantries = await Pantry.find({}).populate("ingredients");
       res.json(pantries[0].ingredients);
     } catch (err) {
       throw err;
--- models/ingredient.js
+++ models/ingredient.js
@@ -1,19 +1,39 @@
-
+const https = require('https')
 const mongoose = require("mongoose");
 
 const IngredientSchema = new mongoose.Schema({
   name: String,
+  pantry: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Pantry' }]
 
 })
+
+
 // IngredientSchema.virtual('type').get(() => {
-//   // this url searches by name
-//   fetch(`https://api.spoonacular.com//food/ingredients/search?query=${this.name}&number=1`)
-//   .then(response => response.json())
-//   .then(data => console.log('id search returns: ', data))
-//   })
-//   // this url searches information by id
-//   // `https://api.spoonacular.com//food/ingredients/:id/information?amount=130&unit=grams`
-//   // return categoryPath
+
+//   const url = `https://api.spoonacular.com/food/ingredients/search?apiKey=a4f1e79a496b4ed6b03ce71e48375963&query=${this.name}&number=1`;
+//   https.get(url, res => {
+//     let data = '';
+//     res.on('data', chunk => {
+//       data += chunk;
+//     });
+//     res.on('end', () => {
+//       data = JSON.parse(data);
+//       console.log(data);
+//     })
+//   }).on('error', err => {
+//     console.log(err.message);
+//   }).end()
+
+// })
+
+  // this url searches by name
+  // fetch(`https://api.spoonacular.com//food/ingredients/search?query=${this.name}&number=1`)
+  // .then(response => response.json())
+  // .then(data => console.log('id search returns: ', data))
+  // })
+  // this url searches information by id
+  // `https://api.spoonacular.com//food/ingredients/:id/information?amount=130&unit=grams`
+  // return categoryPath
 
 const Ingredient = mongoose.model("Ingredient", IngredientSchema);
 
