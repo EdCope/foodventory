6fb6bd082d07cdf4eec932aaf069c1e20f173613
Ed <ed321@hotmail.co.uk>
2022-04-08 16:42:19 +0100

    Changed the add and delete buttons to work with the corresponding pantry
    Co-authored-by: cdoesprogramming <cdoesprogramming@users.noreply.github.com>

--- controllers/pantry.js
+++ controllers/pantry.js
@@ -3,59 +3,44 @@ const Ingredient = require("../models/ingredient");
 const User = require("../models/user");
 
 const PantriesController = {
-  Add: (req, res) => {
-    Pantry.find({})
+  Add: async (req, res) => {
+    const user = await User.findOne({'_id': req.body.user.uid})
+    
+    Pantry.findOne({'_id': user.pantry})
       .exec()
-      .then((pantries) => {
-        if (pantries.length === 0) {
-          const pantry = new Pantry();
-          const ingredient = new Ingredient({
-            name: req.body.ingredient,
-            pantry: pantry,
+      .then((pantry) => {
+        Ingredient.findOne({ pantry: pantry._id, name: req.body.ingredient })
+          .exec()
+          .then((ingredient) => {
+            if (ingredient === null) {
+              const ingredient = new Ingredient({
+                name: req.body.ingredient,
+                pantry: pantry,
+              });
+              ingredient.save((err) => {
+                if (err) {
+                  throw err;
+                }
+              });
+              pantry.ingredients.push(ingredient);
+              pantry.save();
+              res.json({
+                message: `${req.body.ingredient} successfully added`,
+              });
+            } else {
+              res.json({
+                message: `${req.body.ingredient} already in pantry.`,
+              });
+            }
           });
-          ingredient.save();
-          pantry.ingredients.push(ingredient);
-          pantry.save();
-          res.json({ message: `${req.body.ingredient} successfully added` });
-        } else {
-          const pantry = pantries[0];
-          Ingredient.findOne({ pantry: pantry._id, name: req.body.ingredient })
-            .exec()
-            .then((ingredient) => {
-              if (ingredient === null) {
-                console.log("here");
-                const ingredient = new Ingredient({
-                  name: req.body.ingredient,
-                  pantry: pantry,
-                });
-                //console.log('I am here', ingredient.type.then((type) => {type}));
-                ingredient.save((err) => {
-                  if (err) {
-                    throw err;
-                  }
-                });
-                pantry.ingredients.push(ingredient);
-                console.log(pantry.ingredients)
-                pantry.save();
-                res.json({
-                  message: `${req.body.ingredient} successfully added`,
-                });
-              } else {
-                console.log("here");
-                res.json({
-                  message: `${req.body.ingredient} already in pantry.`,
-                });
-              }
-            });
-        }
       });
   },
 
-  Remove: (req, res) => {
-    Pantry.find({})
+  Remove: async (req, res) => {
+    const user = await User.findOne({'_id': req.body.user.uid})
+    Pantry.findOne({'_id': user.pantry})
       .exec()
       .then((pantry) => {
-        console.log(req.body);
         Ingredient.deleteOne({ _id: req.body.ingredient._id }, function (err) {
           res.json({
             message: `${req.body.ingredient.name} successfully removed`,
@@ -68,7 +53,6 @@ const PantriesController = {
     try {
       const user = await User.findOne({'_id': req.params.id}).populate('pantry')
       const pantries = await Pantry.findOne({'_id': user.pantry}).populate("ingredients");
-      console.log('The user is: ', user.pantry)
       res.json(pantries.ingredients);
     } catch (err) {
       throw err;
