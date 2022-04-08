0105265db499b5e6a47fb125cca564eddb9fc1cb
Jack <winterschladen.j@gmail.com>
2022-04-08 11:04:08 +0100

    Added addToFavourites component from a button in recipe list table

---
+++ frontend/src/components/addToFavourites.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+import { useState } from "react"
+
+export const AddToFavourites = (props) => {
+  return (
+    <button id="add-to-favourites" type="button" className="btn btn-warning">
+      Add to Favourites
+    </button>
+  )
+}
---
+++ spec/models/favouriteRecipe.spec.js
@@ -0,0 +1,14 @@
+// const mongoose = require('mongoose');
+// const { isType } = require('validation');
+
+// require("../mongodb_helper");
+
+// describe("FavouriteRecipe model", () => {
+//   beforeEach((done) => {
+//     mongoose.connection.collections.favouriteRecipes.drop(() => {
+//       done();
+//     });
+//   });
+
+
+// })
