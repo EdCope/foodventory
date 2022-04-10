ef481f3dbf0938d988f14402dcb8f9c0aef366ca
Jack <winterschladen.j@gmail.com>
2022-04-10 19:36:30 +0100

    Added initial navbar

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -2,11 +2,36 @@ import { Outlet, Link } from "react-router-dom";
 import { SignOutButton } from "../components/signOutButton";
 import GlobalState from '../contexts/GlobalState';
 import React, { useContext } from 'react';
+import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
 
 export const Layout = () => {
   const [state, setState] = useContext(GlobalState);
 
   return (
+    <>
+    <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="/home">PantryPal</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        {!state.loggedIn && <Nav.Link href="/signup">Sign Up</Nav.Link>}
+        {!state.loggedIn && <Nav.Link href="/login">Log in</Nav.Link>}
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+    <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
     <div className="App">
       <header className="App-header">
         <div>
@@ -30,5 +55,6 @@ export const Layout = () => {
       </header>
       <Outlet />
     </div>
+    </>
   )
 }
