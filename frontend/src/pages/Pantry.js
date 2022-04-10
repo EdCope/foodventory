ef4fb0d5c448ef39ee18a20734d58746507f551e
Ed <ed321@hotmail.co.uk>
2022-04-10 14:00:30 +0100

    Protected Pantry page with a token validity check

--- frontend/src/pages/Pantry.js
+++ frontend/src/pages/Pantry.js
@@ -3,6 +3,7 @@ import { ListIngredients } from "../components/listIngredients";
 import { SearchRecipe } from '../components/searchReceipe';
 import GlobalState from '../contexts/GlobalState';
 import { useNavigate } from "react-router";
+import axios from 'axios';
 
 export const Pantry = () => {
   const [state, setState] = useContext(GlobalState);
@@ -14,6 +15,19 @@ export const Pantry = () => {
       setState(state => ({...state, loggedIn: false}));
       navigate('/');
     }
+    const verifyUser = async () => {
+      const data = await axios.get(`http://localhost:8000/auth/verify`,
+      {headers: {'x-access-token': user.token}})
+      return data.data;
+    }
+    verifyUser().then(res => {
+      if(res.auth === false){
+        localStorage.clear("userdata");
+        setState(state => ({...state, loggedIn: false}));
+        navigate('/');
+        return res;
+      }
+    })
   }, [])
 
   
