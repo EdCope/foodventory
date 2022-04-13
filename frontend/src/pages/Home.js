455f6e7efcecd9c38d9477d17d4ed69d808132d7
Jack <winterschladen.j@gmail.com>
2022-04-13 17:43:12 +0100

    Added homepage login and signup buttons

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,8 @@ import React, {useContext, useEffect} from 'react';
 import GlobalState from '../contexts/GlobalState';
 import { useNavigate } from "react-router";
 import './Home.css'
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 export const Home = () => {
   const [state, setState] = useContext(GlobalState);
@@ -24,9 +26,24 @@ export const Home = () => {
     <p id='landingPage'>PantryPal</p>
     <p id='slogan'>Powered by us, made by you!</p>
     <img src="pantrypal-logo.PNG" alt="pantrypal-logo" id="logo-home" width="300"></img>
-
-     <h4>{state.loggedIn}</h4>
     </div>
+      <div className='row' id='homepage-buttons'>
+        <div className='col 6'>
+          <Link to="/login">
+            <Button id='login-button' className='float-right' variant="outline-light" size="lg">
+              Log in
+            </Button>
+          </Link>
+        </div>
+        <div className='col 6'>
+          <Link to="/signup">
+            <Button id='signup-button' variant="outline-light" size="lg">
+              Sign up
+            </Button>
+          </Link>
+    </div>
+    </div>
+     <h4>{state.loggedIn}</h4>
 
     </>
   )
